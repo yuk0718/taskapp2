5dc03e7ff267a003bd78d4eb5b7a553ede0ad998
yukihide.suda <yuk_da@yahoo.co.jp>
2019-08-03 13:53:16 +0900

    Initial Commit

---
+++ taskapp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp2
+//
+//  Created by 須田幸秀 on 2019/08/03.
+//  Copyright © 2019 yukihide.suda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
